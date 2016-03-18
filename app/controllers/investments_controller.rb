9bb7e3046f1a4aba938f25fb42347e2ce5c71ff4
Matthew Soh <matthewsoh.cw@gmail.com>
2016-03-18 17:55:24 +0800

    Backings can be modified

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,18 +1,48 @@
 class InvestmentsController < ApplicationController
 	def new
-		@investment = Investment.new
 		@project = params[:project_id]
+		@invested = Investment.find_by(user_id: current_user.id, project_id: @project)
+		@investment = Investment.new
+
+		if @invested.present?
+			@investment = @invested.amount
+			@invested = true
+		else
+			@invested = false
+		end
 	end
 
 	def create
 		@investment = current_user.investments.build(investment_params)
+
 		if @investment.save
 			flash[:success] = "Project backed for #{@investment.amount}"
 			@investment.project.calculate_raised
 		else
 			flash[:failure] = "Error in backing project."
 		end
+
 		redirect_to root_url
+	end
+
+	def edit
+		@investment = current_user.investments.find(params[:id])
+		unless @investment.present? 
+			redirect_to new_investment_path(project: params[:project_id])
+		end
+		@project = @investment.project_id
+	end
+
+	def update
+		@investment = current_user.investments.find(params[:id])
+		if @investment.present? and @investment.update_attribute(:amount, investment_params[:amount])
+			flash[:success] = "Project backing changed to #{@investment.amount}"
+			@investment.project.calculate_raised
+			redirect_to root_url
+		else
+			flash[:failure] = "Error in backing project."
+			render 'edit'
+		end
 	end
 	
   protected
