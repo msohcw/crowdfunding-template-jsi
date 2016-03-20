a0f8a4f213d61e23ef61ebc88627922ab6af99b4
Matthew Soh <matthewsoh.cw@gmail.com>
2016-03-20 22:35:26 +0800

    Cleaned up everything with skeleton.css

--- public/assets/application-951e29066789266c3312710f29f65acdd24ca142e8b3a4488871b8f198c80d1f.js
+++ public/assets/application-951e29066789266c3312710f29f65acdd24ca142e8b3a4488871b8f198c80d1f.js
@@ -13040,18 +13040,63 @@ $.externalScript(stripejsSrc).done(function(script, status){
 }).call(this);
 (function() {
   	
-$("#add-credit-card-form").submit(function(event){
-	alert('pause')
-	var $form = $(this);
-	$form.find('button').prop('disabled', true);
-	Stripe.card.createToken($form, stripeResponseHandler);
+$(document).ready(function(){
+	var $form = $("#add-cc-form");
 
-	return false; //prevent default, i.e. prevent form submit
+	$form.submit(function(event){ 
+		var $form = $(this);
+		
+		if($form.find("#stripe_token").val() === ""){ // not yet obtained stripe token
+			initiateStripeRequest($form);
+			// prevent form submit
+			event.preventDefault();
+			return false; 
+		}else{ // stripe token obtained
+			// allow form submit
+			return true;
+		}
+	});
+
+	$("#cc-number").payment('formatCardNumber');
+	$("#cc-cvc").payment('formatCardCVC');
+	$("#cc-expiry").payment('formatCardExpiry');
+
+	$form.on("ajax:success", function(event, data, status, xhr){
+		console.log(data);
+		location.reload(true);
+	}).on("ajax:error", function (event, xhr, status, error) {
+		console.log(error);
+	})
 })
 
+function initiateStripeRequest($form){
+	var expiry = $("#cc-expiry").payment('cardExpiryVal');
+	$form.find('button').prop('disabled', true);
+
+	Stripe.card.createToken({
+		number: $("#cc-number").val(),
+		cvc: $("#cc-cvc").val(),
+		exp_month: expiry.month,
+		exp_year: expiry.year
+	}, stripeResponseHandler);
+	
+	expiry = null;
+}
+
 function stripeResponseHandler(status, response){
-	console.log(status);
 	console.log(response);
+
+	var $form = $("#add-cc-form");
+
+	if(response.error){
+		$("add-payment-method-errors").text(response.error.message);
+		$form.find('button').prop('disabled', false);
+	}else{
+		var token = response.id
+		$form.find("#stripe_token").val(token);
+		token = null; //force clear token
+		$form.submit();
+	}
 }
 ;
 
